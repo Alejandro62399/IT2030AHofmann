0dab0d615aec960a9a8f886c29d0204f1cc4adf0
Alejandro623 <40708721+Alejandro623@users.noreply.github.com>
2019-01-27 20:33:03 -0500

    fix

---
+++ Lab02/Lab02/Controllers/ProductController.vb
@@ -0,0 +1,28 @@
+﻿Imports System.Web.Mvc
+
+Namespace Controllers
+    Public Class ProductController
+        Inherits Controller
+
+        ' GET: Product
+        Function Index() As String
+            Return "Product/Index is displayed"
+        End Function
+
+        ' GET: Products/Browse
+        Function Browse() As String
+            Return "Browse is displayed"
+        End Function
+
+        ' GET: Products/Details/105
+        Function Details() As String
+            Return "Details displayed for id=105 "
+        End Function
+
+        ' GET: Products/Location?zip=44124
+        Function Location() As String
+            Return "Location displayed for zip=44124"
+        End Function
+
+    End Class
+End Namespace
