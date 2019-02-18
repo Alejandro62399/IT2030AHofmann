2fe565f978398ae69eef53ac08e1bc8177f60ee4
Alejandro623 <40708721+Alejandro623@users.noreply.github.com>
2019-02-17 19:23:45 -0500

    lab 5

---
+++ Lab05/EnrollmentApplication/EnrollmentApplication/Models/Enrollment.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EnrollmentApplication.Models
+{
+    public class Enrollment
+    {
+        public virtual int EnrollmentId { get; set; }
+        public virtual int StudentId { get; set; }
+        public virtual int CourseId { get; set; }
+        public virtual int Grade { get; set; }
+        public virtual int StudentObject { get; set; }
+        public virtual int CourseObject { get; set; }
+        public virtual Boolean IsActive { get; set; }
+        public virtual String AssignedCampus { get; set; }
+        public virtual String EnrollmentSemester { get; set; }
+        public virtual int EnrollmentYear { get; set; }
+
+    }
+}
