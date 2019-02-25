bf496d159e74c862256cbba301884730a8bee4c0
Alejandro623 <40708721+Alejandro623@users.noreply.github.com>
2019-02-24 19:25:55 -0500

    lab 6

---
+++ EnrollmentApplication/EnrollmentApplication/Models/Course.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace EnrollmentApplication.Models
+{
+    public class Course
+    {
+        public virtual int CourseId { get; set; }
+        [Required]
+        [StringLength(150)]
+        public virtual int CourseTitle { get; set; }
+        public virtual int CourseDescription { get; set; }
+        [Required]
+        [Range(1, 4)] public int NumCredits { get; set; }
+        public virtual int CourseCredits { get; set; }
+
+    }
+}
---
+++ EnrollmentApplication/EnrollmentApplication/Models/Student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace EnrollmentApplication.Models
+{
+    public class Student
+    {
+        public virtual int StudentId { get; set; }
+        [Required]
+        [StringLength(50)]
+        public virtual int StudentFirstName { get; set; }
+        [Required]
+        [StringLength(50)]
+        public virtual int StudentLastName { get; set; }
+     
+
+    }
+}
